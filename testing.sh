cd6dda06e1eb6cea3b8e3b3cc6e45d58b796d4b0
helal86 <helal86@users.noreply.github.com>
2017-05-02 12:43:34 +0100

    testing script to install apache

---
+++ testing.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Variables
+DBHOST=localhost
+DBNAME=dbname
+DBUSER=dbuser
+DBPASSWD=test123
+SITE=name
+
+echo -e "\n--- Updating packages list ---\n"
+apt-get -qq update
+
+echo -e "\n--- Install base packages ---\n"
+apt-get -y install curl git unzip htop
+echo "done"
+
+echo -e "\n--- Install Apache ---\n"
+apt-get -y install apache2
+echo "done"
+
+echo -e "\n--- Restarting Apache ---\n"
+service apache2 restart
+
+echo "all done"
+exit
+
+
+
+
